dc4dae8be55873fbbf04155aa02bb776a851ad32
Mohammad <mohamadborna1996@gamil.com>
2022-03-23 17:41:19 +0430

    Added Navigation Component: Created Navigation Directory and class Created 2 Fragments Attached to Host Main Activity Added Safe Args Dependency for more safe navigation

---
+++ app/src/main/java/com/example/musicplayer/features/features/PlayedMusic/PlayedMusicFragment.kt
@@ -0,0 +1,24 @@
+package com.example.musicplayer.features.features.PlayedMusic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.musicplayer.R
+
+
+class PlayedMusicFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+
+        return inflater.inflate(R.layout.fragment_played_music, container, false)
+    }
+
+}
