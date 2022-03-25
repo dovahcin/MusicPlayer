3f0ad24a787ece6b3aa9667e5c7ab001b769ebab
Mohammad <mohamadborna1996@gamil.com>
2022-03-25 13:51:35 +0430

    databinding added to fragments programmatically

--- app/src/main/java/com/example/musicplayer/features/features/PlayedMusic/PlayedFragment.kt
+++ app/src/main/java/com/example/musicplayer/features/features/PlayedMusic/PlayedFragment.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.musicplayer.R
+import com.example.musicplayer.databinding.FragmentPlayedBinding
 
 
 class PlayedFragment : Fragment() {
+    private var _binding: FragmentPlayedBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_played, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_played, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/musicplayer/features/features/playlist/PlayListFragment.kt
+++ app/src/main/java/com/example/musicplayer/features/features/playlist/PlayListFragment.kt
@@ -5,13 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.musicplayer.R
+import com.example.musicplayer.databinding.FragmentPlayListBinding
 
 class PlayListFragment : Fragment() {
+    private var _binding: FragmentPlayListBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_play_list, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_play_list, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
